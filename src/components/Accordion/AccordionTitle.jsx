a8c46f52d7cbc176a0a7d87c6fd1198a5f872429
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-10-01 01:43:02 +0600

    fix: fixed accordion

--- src/components/Accordion/AccordionTitle.jsx
+++ src/components/Accordion/AccordionTitle.jsx
@@ -3,13 +3,16 @@ import { ChevronDown } from "lucide-react";
 const AccordionTitle = ({ children, index, setActiveIndex, activeIndex }) => {
     return (
         <div
-            onClick={() => setActiveIndex(index)}
+            onClick={() => {
+                if (index === activeIndex) return setActiveIndex(null)
+                setActiveIndex(index)
+            }}
             className="flex justify-between items-center cursor-pointer mb-3 md:mb-5">
             <h2 className="text-primary text-base font-semibold md:text-[32px] md:font-bold lg:text-5xl">
                 {children}
             </h2>
 
-            <div className={` ${index === activeIndex ?'rotate-180' :''} transition-transform duration-300`}>
+            <div className={` ${index === activeIndex ? 'rotate-180' : ''} transition-transform duration-300`}>
                 <ChevronDown size={32} color="#AAB6B5" />
             </div>
         </div>
--- src/data/ctaData.js
+++ src/data/ctaData.js
@@ -2,8 +2,7 @@ const ctaData = {
     title: "Get In Touch",
     description: "Contact us now to engage our plumbing services, whether you have a commercial project that requires support, or a domestic plumbing task that needs the attention of a trusted professional.",
     button: {
-        text: "Professional Number",
-        link: "tel:your-phone-number",
+        text: "Book a Professional Plumber",
         icon: "→"
     }
 };
